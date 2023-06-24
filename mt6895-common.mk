40c45468912de090bfde8b035b49c0c639d73c74
Daemon Targaryen <131575410+bharath6300000@users.noreply.github.com>
2023-06-24 13:29:18 -0700

    Update mt6895-common.mk

--- mt6895-common.mk
+++ mt6895-common.mk
@@ -99,7 +99,14 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.common@1.0.vendor \
+    android.hardware.camera.device@3.2.vendor \
+    android.hardware.camera.device@3.3.vendor \
+    android.hardware.camera.device@3.4.vendor \
+    android.hardware.camera.device@3.5.vendor \
     android.hardware.camera.device@3.6.vendor \
+    android.hardware.camera.provider@2.4.vendor \
+    android.hardware.camera.provider@2.5.vendor \
     android.hardware.camera.provider@2.6.vendor \
 
 # Config store
